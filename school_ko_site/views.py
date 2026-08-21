02066ea64535533e3452aa5ad74082f7a44b34d1
Apratimmm <Aapratimboy@gmail.com>
2026-08-21 23:33:17 +0545

    new views

--- school_ko_site/views.py
+++ school_ko_site/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from logic.models import AboutSection
+from django.contrib import messages
 def home(request):
-    return render(request, 'home.html')
+    sections = {
+        s.section: s for s in AboutSection.objects.all()
+    }
+
+    context = {
+        "history": sections.get("history"),
+        "principal": sections.get("principal"),
+        "chairperson": sections.get("chairperson"),
+    }
+    return render(request, "home.html", context)
 
 def academics(request):
     return render(request, 'academics.html')
@@ -12,10 +23,14 @@ def calendar(request):
 def contact(request):
     return render(request, 'contact.html')
 
-
 def gallery(request):
     return render(request, 'gallery.html')
 
 def results(request):
     return render(request, 'results.html')
 
+def user_login(request):
+    return render(request, 'login.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
