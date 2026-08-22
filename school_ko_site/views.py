d3c68e316a67ba90a2d88ab93642e09cbe4e065a
Apratimmm <Aapratimboy@gmail.com>
2026-08-23 00:13:40 +0545

    fetch from the database

--- school_ko_site/views.py
+++ school_ko_site/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from logic.models import AboutSection
+from logic.models import *
 from django.contrib import messages
 def home(request):
+
+    contact_info = ContactInfo.objects.all().first()
+
     sections = {
         s.section: s for s in AboutSection.objects.all()
     }
@@ -11,23 +14,43 @@ def home(request):
         "history": sections.get("history"),
         "principal": sections.get("principal"),
         "chairperson": sections.get("chairperson"),
+        "contact": contact_info
     }
     return render(request, "home.html", context)
 
 def academics(request):
-    return render(request, 'academics.html')
+    schools = {
+        s.school: s for s in Academic.objects.all()
+    }
+
+    context = {
+        "primary": schools.get("primary"),
+        "secondary": schools.get("secondary"),
+    }
+    return render(request, 'academics.html', context)
 
 def calendar(request):
     return render(request, 'calendar.html')
 
 def contact(request):
-    return render(request, 'contact.html')
+    contact_info = ContactInfo.objects.first()
+    context = {
+        "contact": contact_info,
+    }
+    return render(request, 'contact.html', context)
 
 def gallery(request):
     return render(request, 'gallery.html')
 
 def results(request):
-    return render(request, 'results.html')
+    yearly_results = YearlyResult.objects.all()[:3]
+    toppers = Topper.objects.all()[:5]
+
+    context = {
+        "yearly_results": yearly_results,
+        "toppers": toppers,
+    }
+    return render(request, "results.html", context)
 
 def user_login(request):
     return render(request, 'login.html')
