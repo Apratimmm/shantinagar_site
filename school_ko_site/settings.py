3c95fe915abbad2b6fa111801f23e9ba9c5cc4e1
Apratimmm <Aapratimboy@gmail.com>
2026-09-20 16:17:11 +0545

    local db connection removed

--- school_ko_site/settings.py
+++ school_ko_site/settings.py
@@ -89,12 +89,7 @@ STORAGES = {
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 CACHES = {
     'default': {
